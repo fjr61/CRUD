582b5d953da5277cd526ec1d24053632946484dc
fjr61 <francisjericho61@gmail.com>
2024-03-01 09:20:00 +0800

    updated

--- createpdf.php
+++ createpdf.php
@@ -2,8 +2,8 @@
 
 use setasign\Fpdi\Fpdi;
 include 'connect.php';
-include 'display.php';
 require_once('vendor/autoload.php');
+
 
 // Start output buffering
 ob_start();
@@ -19,7 +19,15 @@ if (!$result) {
 }
 
 $pdf->AddPage();
+    
+$pdf->SetFont('Arial', '', 12);
+$pdf->SetXY(10, 10); $pdf->Write(0,"ID");
+$pdf->SetXY(40, 10); $pdf->Write(0,"NAME");
+$pdf->SetXY(70, 10); $pdf->Write(0,"CONTACT");
+$pdf->SetXY(110, 10); $pdf->Write(0,"EMAIL");
+$pdf->SetXY(170, 10); $pdf->Write(0,"PASSWORD");
 
+$y=22;
 // Loop through each row in the result set
 while ($row = mysqli_fetch_assoc($result)) {
     // Fetch data from the current row
@@ -29,13 +37,27 @@ while ($row = mysqli_fetch_assoc($result)) {
     $email = $row['Email'];
     $password = $row['Password'];
 
-    // Output data in the PDF
+    $pdf->SetXY(10,$y); $pdf->Write(0,$id);
+    $pdf->SetXY(40, $y); $pdf->Write(0,$name);
+    $pdf->SetXY(70, $y); $pdf->Write(0,$contact);
+    $pdf->SetXY(110, $y); $pdf->Write(0,$email);
+    $pdf->SetXY(170, $y); $pdf->Write(0,$password);
+
+    $y+=10;
+
+    if ($y > 270) {
+        $pdf->AddPage();
+        // Reset Y position and any other necessary parameters for the new page
+        $y = 10;
+    }
+    /* Output data in the PDF
     $pdf->SetFont('Arial', '', 12);
     $pdf->Cell(0, 10, "ID: $id", 0, 1);
     $pdf->Cell(0, 10, "Name: $name", 0, 1);
     $pdf->Cell(0, 10, "Contact: $contact", 0, 1);
     $pdf->Cell(0, 10, "Email: $email", 0, 1);
     $pdf->Cell(0, 10, "Password: $password", 0, 1);
+    */
 }
 
 // Clean (erase) the output buffer and turn off output buffering
